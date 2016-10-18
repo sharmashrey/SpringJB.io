4a5889c1895aaa59da143fe57d36fb6e6f091648
Shreyas <shreyash.sharma@popxo.com>
2016-10-17 18:30:56 -0700

    fixed spring.xml location

--- src/com/shrey/io/DrawingApplication.java
+++ src/com/shrey/io/DrawingApplication.java
@@ -2,6 +2,8 @@ package com.shrey.io;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApplication {
@@ -9,8 +11,10 @@ public class DrawingApplication {
 	
 	public static void main(String[] args){
 	
-	BeanFactory factory = new XmlBeanFactory (new FileSystemResource("spring.xml")) ;	
-		Triangle tri = (Triangle) factory.getBean("triangle");
+		//old deprecated resource
+		//	BeanFactory factory = new XmlBeanFactory (new FileSystemResource("spring.xml")) ;	
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle tri = (Triangle) context.getBean("triangle");
 		tri.draw();
 	}
 }
